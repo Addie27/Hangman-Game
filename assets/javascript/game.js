1af13360b6cfe91d6be95c35815c0fd66583498c
Adeline Johnson <groblewski.adeline@gmail.com>
2018-02-18 21:50:41 -0500

    Pseudocode

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,21 @@
 
-//document.getElementById("start").onclick = function (event) {
-    //window.location.href = "play.html";
-//};
+//Problem 1: Wins: set to 0 
+
+//Problem 2: Create answers
+
+//Problem 3: Display 1 answer
+
+//Problem 4: User clicks letters
+
+//Problem 5: As user clicks, display letters either in answer or in letters used section
+
+//Problem 6: Number of guesses remaining 
+
+//Problem 7: Once all correct letters have been input, user wins and score increases
+
+//Problem 8: If user gets uses all guesses remaining, user loses and hangman is displayed. Play again button? 
+
+//Problem 9: Game automatically chooses new answer and user plays again
+
+
+
